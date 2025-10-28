ece4928ada6c3b939bd6c1cd05a4f40ac41a6edb
Axel Thornberg <axel@thornberg.se>
2025-10-28 11:34:09 +0100

    Removed unused leftover function

--- visualize.py
+++ visualize.py
@@ -44,35 +44,3 @@ def visualize_step(step, label = ""):
         font.render_to(window, (20, 20), label, (255, 255, 255))
 
     pygame.display.flip()
-
-# def visualize_simulation(simulation):
-#     init()
-#     # Get max x and y for scaling
-#     max_x = max(obj["position"][0] for step in simulation for obj in step)
-#     max_y = max(obj["position"][1] for step in simulation for obj in step)
-#     min_x = min(obj["position"][0] for step in simulation for obj in step)
-#     min_y = min(obj["position"][1] for step in simulation for obj in step)
-#
-#     print(f"Max X: {max_x}, Max Y: {max_y}, Min X: {min_x}, Min Y: {min_y}")
-#
-#     simulation = simulation[::3600]
-#
-#     for step in simulation:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 is_running = False
-#
-#         window.fill((0, 0, 0))  # Clear the screen with black
-#
-#         # Draw each object
-#         for obj in step:
-#             tmp_obj = copy.deepcopy(obj)
-#             tmp_obj["position"] = (
-#                 (tmp_obj["position"][0] - min_x) / (max_x - min_x) * window_size[0],
-#                 (tmp_obj["position"][1] - min_y) / (max_y - min_y) * window_size[1]
-#             )
-#             x, y = int(tmp_obj["position"][0]), int(tmp_obj["position"][1])
-#             pygame.draw.circle(window, tmp_obj["color"], (tmp_obj["position"][0], tmp_obj["position"][1]), tmp_obj["radius"])
-#
-#         pygame.display.flip()
-#         # pygame.time.wait(1)  # Pause for a short duration to visualize the step
